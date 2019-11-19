9951929c882819ce8ea0427677129e996eee4558
AlejakSoto <alejaksoto@gmail.com>
2019-11-19 16:33:19 -0500

    js
    ok

---
+++ index.js
@@ -0,0 +1,10 @@
+
+var apellido = 'sanchez';
+var nombre ;
+nombre = 'probando'
+var nombreenMayusculas = nombre.toUpperCase();
+var minisculas = apellido.toLowerCase();
+
+var primerLetra = nombre.charAt(7);
+var cantidadLetras = nombre.length;
+console.log('nombre '+ nombre +' '+ apellido );
