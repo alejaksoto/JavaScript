77e81c3f68821539b2a5b4c6dbb1f94388332ea8
alejaksoto <alejaksoto@gmail.com>
2020-06-20 16:22:44 -0500

    end excersice condicionales

--- Poo/python/excersice/cond10.py
+++ Poo/python/excersice/cond10.py
@@ -3,20 +3,25 @@
 seleccion =int(input('selecciona 1:vegetariana o 2:No vegetarianos'))
 
 if seleccion ==1:
-    print('El cliente selecciono Vegetariana con')
+    
     ingredietes = int (input('1:Pimiento ,2: tofu'))
     if ingredietes ==1:
-        print(' con Pimiento')
+        print('El cliente selecciono Vegetariana con')
+        print(' Pimiento')
     else:
-        print(' con Tofu')
+        print('El cliente selecciono Vegetariana con')
+        print(' Tofu')
 else :
-    print('El cliente selecciono Carne con')
+    
     ingredietes = int (input('1:Peperoni, 2:Jamón, 3:Salmón.'))
-     if ingredietes ==1:
-        print(' con peperoni')
-    elif:
-        print(' con Jamon')
+    if ingredietes ==1:
+        print('El cliente selecciono Carne con')
+        print(' peperoni')
+    elif ingredietes ==2:
+        print('El cliente selecciono Carne con')
+        print(' Jamon')
     else:
-        print(' con salmon')
+        print('El cliente selecciono Carne con')
+        print(' salmon')
 
 
